cff64f3247ed9ef32a22225445a714e3a32a50fc
Emil Diaz <emildiaz242@yahoo.com>
2020-07-21 18:46:18 -0400

    quick test for cronjob

--- cronjobs.py
+++ cronjobs.py
@@ -3,5 +3,5 @@ import twitter_bot
 
 #creating a time scheduler to run code every two hours
 scheduler = BlockingScheduler()
-scheduler.add_job(twitter_bot.main, 'interval', minutes=120)
+scheduler.add_job(twitter_bot.main, 'interval', minutes=1)
 scheduler.start()
