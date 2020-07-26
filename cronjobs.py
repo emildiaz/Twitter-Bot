03135482b90148bc43d32f65d792539f0a9f41f7
Emil Diaz <emildiaz242@yahoo.com>
2020-07-26 16:01:32 -0400

    removed first call to main in cronjobs

--- cronjobs.py
+++ cronjobs.py
@@ -1,9 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import twitter_bot
 
-#first call twit-bot once
-twitter_bot.main()
-
 #creating a time scheduler to run code every two hours
 scheduler = BlockingScheduler()
 scheduler.add_job(twitter_bot.main, 'interval', hours=2)
